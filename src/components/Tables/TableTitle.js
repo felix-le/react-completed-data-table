bfe851a1efe85e41fcff486562f657077a9fd6f9
Viet Anh Le <mc.vietanh@gmail.com>
2022-07-10 00:06:55 -0400

    rename app

--- src/components/Tables/TableTitle.js
+++ src/components/Tables/TableTitle.js
@@ -7,15 +7,6 @@ function TableTitle({ tableTitle, description, handleAddTodo }) {
         <h1 className='text-xl font-semibold text-gray-900'>{tableTitle}</h1>
         <p className='mt-2 text-sm text-gray-700'>{description}</p>
       </div>
-      <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
-        <button
-          type='button'
-          className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'
-          onClick={handleAddTodo}
-        >
-          Add todo
-        </button>
-      </div>
     </div>
   );
 }
